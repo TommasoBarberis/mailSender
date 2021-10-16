315a52e325bef2f0ecba603fab468306f91ea67a
Tommaso <tommasobarberis98@gmail.com>
2021-10-16 14:35:30 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# mailSender
-Script to send automatically using gmail api
+Short python script that can be used in a bash script in order to be notfied when a long task end.
+Command line exemple:
+
+```
+python3 sender.py subject receiver msg_text
+```
+
+* subject: [string] it will be the subject of the mail
+* receiver: [string] such as "address1@domain.com, address2@domain.com" or a [text file] with several address (one address for line, no comma divided)
+* msg_text: [string] it will be the text of the mail
---
+++ sender.py
@@ -0,0 +1,31 @@
+import sys
+import os.path
+from Google import Create_Service
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+CLIENT_SECRET_FILE = "credentials.json"
+API_NAME = 'gmail'
+API_VERSION = 'v1'
+SCOPES = ['https://mail.google.com/']
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+emailMsg = sys.argv[1]
+mimeMessage = MIMEMultipart()
+
+if type(sys.argv[2]) == str:
+    receivers = sys.argv[2]
+if os.path.isfile(sys.argv[2]):
+    with open(sys.argv[2]) as file:
+        lines = file.readlines()
+        receivers = ", ".join(str(address.strip("\n")) for address in lines)
+
+mimeMessage['to'] = receivers
+mimeMessage['subject'] = sys.argv[3]
+mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+
+message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
+# print(message)
